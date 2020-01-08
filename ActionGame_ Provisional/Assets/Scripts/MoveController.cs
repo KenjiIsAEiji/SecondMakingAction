5208fb662161994e825be1ac87388d98cc3555e9
eijiMiyazawa <b1800722@planet.kanazawa-it.ac.jp>
2020-01-08 16:50:44 +0900

    簡単な移動を実装

---
+++ ActionGame_ Provisional/Assets/Scripts/MoveController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class MoveController : MonoBehaviour
+{
+    CharacterController characterController;
+    Vector3 MoveVector = Vector3.zero;
+
+    [SerializeField] float MoveSpeed = 10.0f;
+    [SerializeField] float Gravity = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        if (characterController.isGrounded)
+        {
+            MoveVector.x = moveX * MoveSpeed;
+            MoveVector.z = moveZ * MoveSpeed;
+        }
+
+        MoveVector.y -= Gravity * Time.deltaTime;
+
+        characterController.Move(transform.TransformDirection(MoveVector * Time.deltaTime));
+    }
+}
