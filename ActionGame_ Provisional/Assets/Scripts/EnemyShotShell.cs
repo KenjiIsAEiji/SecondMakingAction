c70c38c6e3918154c0801dc3571521b1c58829cb
ikeno <b1819861@planet.kanazawa-it.ac.jp>
2020-02-25 16:09:12 +0900

    プレイヤーが見える状態になったら自動で射撃

---
+++ ActionGame_ Provisional/Assets/Scripts/EnemyShotShell.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShotShell : MonoBehaviour
+{
+    public GameObject enemyShellPrefab;
+    public float shotSpeed;
+    public Transform target;
+    public float rate;
+    public float shootInterval=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Shoot");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootInterval += Time.deltaTime;
+        if(shootInterval >= rate)
+        {
+            Shoot();
+            shootInterval = 0.0f;
+        }
+    }
+    private void Shoot()
+    {
+            GameObject enemyShell = Instantiate(enemyShellPrefab, transform.position, Quaternion.identity);
+
+            Rigidbody enemyShellRb = enemyShell.GetComponent<Rigidbody>();
+
+            // forwardはZ軸方向（青軸方向）・・・＞この方向に力を加える。
+            enemyShellRb.AddForce(transform.forward * shotSpeed);
+
+            Destroy(enemyShell, 3.0f);
+    }
+}
