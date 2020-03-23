2bd70db36d7ef51236293b3d4f9295c9a40ed27c
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2020-03-23 17:35:35 +0900

    DOTween導入

--- ActionGame_ Provisional/Assets/Scripts/HealthBar.cs
+++ ActionGame_ Provisional/Assets/Scripts/HealthBar.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+
+    RectTransform rectTransform;
+
     [SerializeField] Gradient colorGradient;
     [SerializeField] Image fill;
+    [SerializeField] Color FlshColor;
+    [SerializeField] Image animatedBorder;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
 
     public void SetMaxHealth(float health)
     {
@@ -20,5 +31,25 @@ public class HealthBar : MonoBehaviour
     {
         slider.value = health;
         fill.color = colorGradient.Evaluate(slider.normalizedValue);
+
+        if (animatedBorder != null)
+        {
+            rectTransform.DOShakePosition(1f, 10f);
+
+            Sequence sequence = DOTween.Sequence();
+
+            sequence.Append(
+                animatedBorder.rectTransform.DOPunchScale(new Vector3(1.2f,1.2f),0.2f)
+            );
+
+            sequence.Join(
+                animatedBorder.DOFade(1f, 0.2f).SetEase(Ease.InFlash).SetLoops(3)
+            );
+
+            sequence.Play().OnComplete(() => {
+                animatedBorder.color = FlshColor;
+                animatedBorder.rectTransform.localScale = Vector3.one;
+            });
+        }
     }
 }
