b96a70bc11331a8509910fc4bce93094fde51892
eijiMiyazawa <b1800722@planet.kanazawa-it.ac.jp>
2020-04-07 14:50:22 +0900

    SpawnControllerを作成

---
+++ ActionGame_ Provisional/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    enum GameState
+    {
+        Ready,
+        Play,
+        End
+    }
+
+    [SerializeField] GameState gameState;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        gameState = GameState.Ready;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        switch (gameState)
+        {
+            case GameState.Ready:
+
+                break;
+
+            case GameState.Play:
+
+                break;
+
+            case GameState.End:
+
+                break;
+
+        }
+    }
+}
